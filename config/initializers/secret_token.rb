8f531640d9dd520b502b4649204c8de39e12451c
Tomas Alvarez <tomas.alvarez.me@gmail.com>
2013-07-02 14:22:11 -0400

    created tropo test app with figaro gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tropotest::Application.config.secret_token = '02e35d204305d1ecf5a2aedb7ddbc35fa1b5e7b15f6b4d3160e3562c07a44e47fb881a485ac646f7f8014b393dc9bffaa8319e8c97fbd69c4823908cbeecd8b6'
